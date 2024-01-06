3be944b5804fb29b842ecf7d96a7222ed76f4508
Ali Turkaman <79016171+aliturkamanjs@users.noreply.github.com>
2024-01-06 09:34:33 +0330

    fix: minor change

--- components/ui/input/index.tsx
+++ components/ui/input/index.tsx
@@ -4,37 +4,22 @@ import { Case, Switch, cn } from '@spline/lib'
 import React, { InputHTMLAttributes, ReactNode, forwardRef, useState } from 'react'
 
 interface IProps extends InputHTMLAttributes<HTMLInputElement> {
-  className?: string
-  parentClassName?: string
   leftContent?: ReactNode | undefined
-  leftContentClassName?: string
   error?: string | undefined
   onInput?: (event: React.FormEvent<HTMLInputElement>) => void
 }
 
 const Input = forwardRef<HTMLInputElement, IProps>(
-  (
-    {
-      className = '',
-      parentClassName,
-      leftContent,
-      leftContentClassName,
-      error,
-      type,
-      onInput: parentOnInput,
-      ...rest
-    }: IProps,
-    ref,
-  ) => {
-    const [iconVisibility, setIconVisibility] = useState<boolean>(false)
+  ({ leftContent, error, type, onInput: parentOnInput, ...rest }: IProps, ref) => {
+    const [leftContentVisibility, setLeftContentVisibility] = useState<boolean>(false)
 
     const handleOnInput = (event: React.FormEvent<HTMLInputElement>) => {
       const inputElement = event.target as HTMLInputElement
       // Add your additional logic here
       if (!inputElement.value.length) {
-        setIconVisibility(false)
+        setLeftContentVisibility(false)
       } else {
-        setIconVisibility(true)
+        setLeftContentVisibility(true)
       }
 
       // Call the parent's onInput event handler
@@ -44,14 +29,13 @@ const Input = forwardRef<HTMLInputElement, IProps>(
     }
 
     return (
-      <div className={cn('group font-[YekanBakh-Regular] text-xl', parentClassName)}>
+      <div className="group font-[YekanBakh-Regular] text-xl">
         <div className="relative">
           <input
             ref={ref}
             autoComplete="true"
             className={cn(
               'w-full rounded-xl border border-[#D9D9D9] bg-[#FFFFFF] px-3 py-2 text-[#A7A7A7] shadow-[0px_-3px_0px_0px_#E0E0E0_inset] outline-none transition-all group-hover:bg-[#f8fafc]',
-              className,
               { 'border-[#FB7185] shadow-[0px_3px_0px_0px_#FB7185]': error },
             )}
             type={type}
@@ -62,9 +46,8 @@ const Input = forwardRef<HTMLInputElement, IProps>(
           <div
             className={cn(
               'pointer-events-none absolute left-3 top-1/2 -translate-y-1/2 transform cursor-pointer text-[#BCBCBC]',
-              leftContentClassName,
               {
-                hidden: iconVisibility,
+                hidden: leftContentVisibility,
               },
             )}
           >
