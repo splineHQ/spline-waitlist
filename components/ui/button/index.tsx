4b4fd4bf83e4fe88569274671a19dbd765b68da8
Matin turkaman <79017406+matinturkaman@users.noreply.github.com>
2024-01-06 09:34:26 +0330

    fix: minor change remove unnecessary props for button

--- components/ui/button/index.tsx
+++ components/ui/button/index.tsx
@@ -5,28 +5,19 @@ import { ButtonHTMLAttributes, ReactNode } from 'react'
 import { twMerge as twMergeOriginal } from 'tailwind-merge'
 
 export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  className?: string | undefined
   children: ReactNode
   disabled?: boolean
   loading?: boolean
-  outline?: boolean
-  transparent?: boolean
   fullSized?: boolean
   rtl?: boolean
-  lucid?: boolean
-  spanClassname?: string
 }
 
 function Button({
   children,
-  className,
   disabled = false,
   loading = false,
-  outline = false,
-  transparent = false,
   fullSized = false,
   rtl = false,
-  lucid = false,
   ...rest
 }: IButtonProps) {
   return (
@@ -37,11 +28,7 @@ function Button({
         clsx(
           'flex flex-row-reverse items-center justify-center space-x-1 rounded-xl border border-[#D9D9D9] bg-[#232323] px-2 py-1.5 font-[yekan-regular] text-xl text-white shadow-[0px_-4px_0px_0px_#424457_inset] transition-all',
           { 'flex-row space-x-reverse': rtl },
-          { 'bg-transparent': outline },
           { 'w-full': fullSized },
-          { 'bg-opacity-25 hover:bg-opacity-40': lucid },
-          { 'border-transparent bg-transparent hover:bg-inherit': transparent },
-          className,
         ),
       )}
       {...rest}
