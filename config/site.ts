54f6b30cc263ffbd8e4d292cac4b373194f5475d
Ali Turkaman <79016171+aliturkamanjs@users.noreply.github.com>
2024-01-07 10:34:07 +0330

    feat: add SEO

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from '@spline/types'
+
+export const siteConfig: SiteConfig = {
+  name: 'Spline',
+  description: 'با اسپلاین به راحتی طرح های گرافیکی حرفه ای بسازید.',
+  url: 'https://spline.ir/',
+  ogImage: 'https://res.cloudinary.com/dhgkzdm23/image/upload/v1704609946/spline/z9ebjbibkvtgletqxipg.jpg',
+  links: {
+    twitter: 'https://twitter.com/spline_ir',
+    instagram: 'https://www.instagram.com/spline_ir',
+  },
+}
--- types/submisson/index.ts
+++
@@ -1,4 +0,0 @@
-export interface formDataModel {
-  name: string
-  email: string
-}
