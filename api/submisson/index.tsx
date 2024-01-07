11cfa336b268380fdf2ff341a29acac0a5a1d108
Ali Turkaman <79016171+aliturkamanjs@users.noreply.github.com>
2024-01-07 11:56:24 +0330

    fix: minor change

--- api/submisson/index.tsx
+++ api/submisson/index.tsx
@@ -2,7 +2,7 @@ import { formDataModel } from '@spline/types'
 import axios, { AxiosInstance, AxiosResponse } from 'axios'
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'https://formcarry.com/s',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
